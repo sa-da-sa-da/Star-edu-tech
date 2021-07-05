0fcf2e2e214fbca6fe61666d0fbb4ed92ca0e3f1
Sakaay <56340246+sa-da-sa-da@users.noreply.github.com>
2021-07-05 21:20:54 +0800

    Update favorite.js

--- miniprogram/pages/user/favorite/favorite.js
+++ miniprogram/pages/user/favorite/favorite.js
@@ -1,106 +1,63 @@
+import Dialog from '@vant/weapp/dialog/dialog';
+const db = wx.cloud.database({
+  env: "demo-8gww0qau03b0af5a"
+})
 const app = getApp()
-import api from "../../../utils/api.js"
-import task from "../../../utils/request.js"
-
-
-
-
-
-
-
-// 松阳
-Page({
-
-  /**
-   * 页面的初始数据
-   */
+Component({
   data: {
     loding: true,
     active: 0,
+    type: "小程序",
     tab: [
       "收藏",
       "点赞",
     ]
-
+  },
+  pageLifetimes: {
+    show: function () {
+      this.post()
+    },
+  },
+  created() {
+    this.post()
   },
 
-  initial() {
-    task.Tree_get( api.GET_Article_collect).then(res => {
-      console.log(res)
-      this.setData({
-        xw_list: res,
-        loding: false
+  methods: {
+    post() {
+      db.collection('collect').limit(10).orderBy('add_time', 'asc').get().then(res => {
+        this.setData({
+          xw_list: res.data,
+          loding: false
+        })
       })
-    })
-  },
-  onChange(event) {
-    this.setData({
-      loding: true
-    })
-    var db2
-    if (event.detail.title == "收藏") {
-      db2 = api.GET_Article_collect
-    } else {
-      db2 = api.GET_Article_statr
-    }
-    task.Tree_get( db2).then(res => {
+    },
+    onChange(event) {
       this.setData({
-        xw_list: res,
-        loding: false
+
+        loding: true
       })
-    })
-  },
+      var db2
+      if (event.detail.title == "收藏") {
+        db2 = "collect"
+      } else {
+        db2 = 'statr'
+      }
+      db.collection(db2).limit(10).orderBy('add_time', 'asc').get().then(res => {
+        this.setData({
+          xw_list: res.data,
+          loding: false
+        })
+      })
 
-  onLoad: function (options) {
-this.initial()
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    tz: function (e) {
+      console.log(e.currentTarget.dataset.id)
+      wx.navigateTo({
+        url: "../../../pages/" + e.currentTarget.dataset.a + "?id=" + e.currentTarget.dataset.id
+      })
+    },
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
   }
 })
